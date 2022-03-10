0c3418616010bf980b51d9b035cf04f4bff3c0e7
RosaleeKnight <rtknight92@outlook.com>
2022-03-09 21:49:18 -0600

    Edit palindrome-checker

--- javascript/palindrome-checker.js
+++ javascript/palindrome-checker.js
@@ -1,19 +1,8 @@
-function reverse(str) {
-	let rev_str = "";
-	    for (let i = str.length-1; i >= 0; i--) {
-	        rev_str+= str[i];
-	}
-	return rev_str;
+function palindrome(string) {
+    const original = string.replace(/\s/g,'').toLowerCase();
+    const reverse = original.split('').reverse().join('');
+    return original === reverse;
 }
-function is_palindrome(str) {
-	reverse_str = reverse(str);
-	    if (reverse_str === str) {
-	        console.log("passed string is palindrome ");
-	    } else {
-	        console.log("passed string is not palindrome")
-	    }
-}
-
-let test = "racecar";
-is_palindrome(test);
+  
+  console.log(palindrome('race car'));
 
