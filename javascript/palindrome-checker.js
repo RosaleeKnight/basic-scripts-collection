540889d1a88aa733412934ed633d95cd1628888b
RosaleeKnight <rtknight92@outlook.com>
2022-03-09 21:28:14 -0600

    Edit palindrome-checker format

--- javascript/palindrome-checker.js
+++ javascript/palindrome-checker.js
@@ -1,6 +1,6 @@
 function reverse(str) {
 	let rev_str = "";
-	    for (let i = str.length-1 ;i >= 0 ;i--) {
+	    for (let i = str.length-1; i >= 0; i--) {
 	        rev_str+= str[i];
 	}
 	return rev_str;
